238eeeb890ec93187a3a99aa840dadfc26caec11
Baptiste Jamin <baptistejamin@gmail.com>
2018-03-08 13:53:08 +0100

    find_people_profile_by_email method

--- lib/resources/website.rb
+++ lib/resources/website.rb
@@ -193,6 +193,12 @@ module Crisp
       return @parent.head(self._url_people("profile", website_id, people_id, ""))
     end
 
+    def find_people_profile_by_email(website_id, email)
+      filter = [{:model => "people", :criterion => "email", :operator => "eq", query: [email]}]
+      profiles = @parent.get(self._url_website(website_id, "/people/profiles/1?search_filter=%s" % CGI.escape(filter.to_json)))
+      return profiles[0]
+    end
+
     def get_people_profile(website_id, people_id)
       return @parent.get(self._url_people("profile", website_id, people_id, ""))
     end
